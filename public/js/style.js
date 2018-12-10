7cd5032273c85185b047fd0599858ccbf40a124c
a.humeidh <a.humeidh@gmail.com>
2018-12-11 00:33:09 +0500

    first commit

---
+++ public/js/style.js
@@ -0,0 +1,104 @@
+
+
+$(document).ready(function() {
+ 
+  $("#owl-demo").owlCarousel({
+ 
+      navigation : true, 
+	  // Show next and prev buttons
+      slideSpeed : 400,
+      paginationSpeed : 400,
+	    autoPlay : 4500,
+      singleItem:true,
+      loop:true
+      // "singleItem:true" is a shortcut for:
+      // items : 1, 
+      // itemsDesktop : false,
+      // itemsDesktopSmall : false,
+      // itemsTablet: false,
+      // itemsMobile : false
+ 
+  });
+ 
+});
+
+
+$(document).ready(function() {
+              var owl = $('#owl-test');
+              owl.owlCarousel({
+                items: 2,          
+                loop:true,
+                margin:10,
+                autoPlay:3800,
+                autoplayTimeout:1000,
+                autoplayHoverPause:true,
+
+              });
+             
+            });
+
+
+
+
+
+
+
+//scroll!
+
+jQuery(document).ready(function($){
+	// browser window scroll (in pixels) after which the "back to top" link is shown
+	var offset = 300,
+		//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+		offset_opacity = 1200,
+		//duration of the top scrolling animation (in ms)
+		scroll_top_duration = 700,
+		//grab the "back to top" link
+		$back_to_top = $('.cd-top');
+
+	//hide or show the "back to top" link
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('cd-fade-out');
+		}
+	});
+
+	//smooth scroll to top
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
+
+});
+
+
+
+
+
+
+
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
+
+
+
+
+
---
+++ resources/views/index.blade.php
@@ -0,0 +1,318 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Promoters Agency</title>
+
+    <!-- Bootstrap -->
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('css/style.css')}}" rel="stylesheet">
+    <link href="{{asset('css/owl.carousel.css')}}" rel="stylesheet">
+	  <link rel="stylesheet" href="{{asset('css/owl.theme.css')}}">
+
+    <link rel="apple-touch-icon" sizes="57x57" href="{{asset('img/fav/apple-icon-57x57.png')}}">
+    <link rel="apple-touch-icon" sizes="60x60" href="{{asset('img/fav/apple-icon-60x60.png')}}">
+    <link rel="apple-touch-icon" sizes="72x72" href="{{asset('img/fav/apple-icon-72x72.png')}}">
+    <link rel="apple-touch-icon" sizes="76x76" href="{{asset('img/favapple-icon-76x76.png')}}">
+    <link rel="apple-touch-icon" sizes="114x114" href="{{asset('img/fav/apple-icon-114x114.png')}}">
+    <link rel="apple-touch-icon" sizes="120x120" href="{{asset('img/fav/apple-icon-120x120.png')}}">
+    <link rel="apple-touch-icon" sizes="144x144" href="{{asset('img/fav/apple-icon-144x144.png')}}">
+    <link rel="apple-touch-icon" sizes="152x152" href="{{asset('img/fav/apple-icon-152x152.png')}}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('img/fav/apple-icon-180x180.png')}}">
+    <link rel="icon" type="image/png" sizes="192x192"  href="{{asset('img/fav/android-icon-192x192.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('img/favfavicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="96x96" href="{{asset('img/fav/favicon-96x96.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('img/fav/favicon-16x16.png')}}">
+    <link rel="manifest" href="{{asset('img/fav/manifest.json')}}">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="img/fav/ms-icon-144x144.png">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- FONTS -->
+    <link rel="stylesheet" href="{{asset('css/font-awesome.min.css')}}">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400" rel="stylesheet">
+
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body id="page-top">
+
+
+
+   <!-- nav main -->
+   <nav class="navbar navbar-default">
+
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-3">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"><img src="img/logo3.jpg" class="img-responsive"></a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="navbar-collapse-3" >
+          <ul class="nav navbar-nav navbar-right" >
+            <li class="active"><a href="{{url('/')}}">HOME</a></li>
+            <li><a href="#">ABOUT </a></li>
+            <li><a href="#">FIND A JOB</a></li>
+            <li><a href="#">CONTACT</a></li>
+            <li><a class="style1" href="#">LOGIN</a></li>
+            <li><a class="style2" href="#">SIGN UP</a></li>
+          </ul>
+
+        </div><!-- /.navbar-collapse -->
+
+  </nav><!-- /.navbar -->
+<!--2nd navbar-->
+
+
+
+
+
+<!--BANNER-->
+
+<div class="block2 bg1">
+<div class="container">
+<div class="row">
+
+      <div class="col-xs-12" align="center">
+
+      <h1 class="blue">Find me a Job!</h1>
+      <h4>We are here to serve you with the best of your needs.</h4>
+      <br>
+
+      <div class="col-xs-12 col-lg-2" >
+      </div>
+
+      <div class="col-xs-12 col-lg-8" align="center">
+       <div id="custom-search-input">
+                <div class="input-group col-md-12">
+                    <input type="text" class="form-control input-lg" placeholder="What are you looking for?" />
+                    <span class="input-group-btn">
+                        <button class="btn btn-info btn-lg" type="button">
+                            <i class="glyphicon glyphicon-search"></i>
+                        </button>
+                    </span>
+                </div>
+            </div>
+      </div>
+
+      <div class="col-xs-12 col-lg-2" >
+      </div>
+
+
+
+      </div>
+
+</div>
+</div>
+</div>
+<!--ENDS BANNER-->
+
+
+
+<!-- CLIENTS -->
+<div class="block " >
+<div class="container">
+<div class="row">
+
+          <div class="col-lg-12 col-xs-12 " align="center">
+              <h1 > How Promoters Agency Works  </h1>
+              <hr class="sep">
+
+
+              <div class="row step_bg">
+                              <div class="col-xs-12 col-lg-6 step side" align="center">
+                                        <h3 >Future Employees</h3>
+                                        <br>
+
+                                        <div class="row ">
+                                            <div class="col-xs-6 step2" >
+                                                <a class="button1" href="">STEP 1</a>
+                                            </div>
+
+                                            <div class="col-xs-6 step2">
+                                               <a class="button1" href="">STEP 2</a>
+                                            </div>
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-xs-6 step2">
+                                                <a class="button1" href="">STEP 3</a>
+                                            </div>
+
+                                            <div class="col-xs-6 step2" >
+                                              <a class="button1" href="">STEP 4</a>
+                                            </div>
+                                        </div>
+                              </div>
+
+
+                              <div class="col-xs-12 col-lg-6 step" align="center">
+                                            <h3 >Companies</h3>
+                                             <br>
+
+                                            <div class="row">
+                                            <div class="col-xs-6 step2">
+                                                <a class="button2" href="">STEP 1</a>
+                                            </div>
+
+                                            <div class="col-xs-6 step2">
+                                                <a class="button2" href="">STEP 2</a>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-xs-6 step2">
+                                                <a class="button2"href="">STEP 3</a>
+                                            </div>
+
+                                            <div class="col-xs-6 step2">
+                                              <a  class="button2" href="">STEP 4</a>
+                                            </div>
+                                        </div>
+                              </div>
+
+
+                 </div><!-- row-->
+          </div><!-- col 12-->
+</div>
+</div>
+</div>
+<!-- END CLIENTS-->
+
+
+<!-- PARTNERS-->
+<div class="block bg2 " id="contact">
+<div class="container">
+<div class="row">
+
+       <div class="col-lg-12 col-xs-12 " align="center">
+              <h1 > Companies who work with us </h1>
+              <hr class="sep">
+
+          <a href="#"><img src="img/brands/1.png" class="brands"></a>
+          <a href="#"><img src="img/brands/2.png" class="brands"></a>
+          <a href="#"><img src="img/brands/3.png" class="brands"></a>
+          <a href="#"><img src="img/brands/4.png" class="brands"></a>
+          <a href="#"><img src="img/brands/4.png" class="brands"></a>
+          <a href="#"><img src="img/brands/2.png" class="brands"></a>
+          <a href="#"><img src="img/brands/3.png" class="brands"></a>
+          <a href="#"><img src="img/brands/1.png" class="brands"></a>
+
+
+
+        </div>
+
+
+</div>
+</div>
+</div>
+<!-- PARTNERS-->
+
+<!-- CONTACT-->
+<div class="block " id="contact">
+<div class="container">
+<div class="row">
+
+      <div class="col-lg-12" align="center">
+          <h1>CONTACT US</h1>
+           <hr class="sep">
+
+            <div class="row">
+
+                  <div class="col-xs-12 col-lg-8">
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3979.2331169270187!2d73.50278381471183!3d4.174521847157637!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3b3f7e53961aad5f%3A0xcd43c9f86508df95!2sMetroflex+Gym+-+Hardcore+Training+Center!5e0!3m2!1sen!2smv!4v1536302991056" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+                  </div>
+
+                  <div class="col-xs-12 col-lg-4 contactus" >
+                  <h3 class="blue">Find Us</h3>
+                  <p>H. Address, Level </p>
+                  <p>Road name</p>
+                  <p>Male, Rep of Maldives</p>
+
+                  <br>
+                   <h3 class="blue">Reach Us</h3>
+                  <p>+960 333 0000 | +960 777 0000 </p>
+                  <p>info@email.com</p>
+
+
+
+                  </div>
+
+            </div><!-- row-->
+
+      </div><!-- col 12-->
+
+</div>
+</div>
+</div>
+<!-- END CONTACT-->
+
+
+
+
+<!-- FOOTER 2 -->
+<div class="footer2 bg3">
+<div class="container">
+<div class="row">
+
+        <div class="col-lg-12" align="center">
+        <img src="img/logo1.jpg" width="150px">
+        <br><br>
+
+              <div class="row">
+
+                  <div class="col-xs-12 col-lg-4">
+                  <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                  <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                  <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                  </div>
+
+                    <div class="col-xs-12 col-lg-4">
+                    <p class="white">CopyRight @ Promoters Agency | 2018</p>
+
+                    </div>
+
+                    <div class="col-xs-12 col-lg-4">
+                   <p> <a href="#">Home</a> | <a href="#">About</a> | <a href="#">Find Job</a> | <a href="#">Contact</a> </p>
+
+                    </div>
+
+              </div>
+
+</div>
+</div>
+</div>
+
+<!-- END FOOTER 2 -->
+
+
+<!---SCROLL TOP -->
+      <a href="#top" class="cd-top"><i class="fa fa-angle-up"></i></a>
+<!---SCROLL TOP -->
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="{{asset('js/jquery-3.1.1.min.js')}}"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('js/style.js')}}"></script>
+    <script src="{{asset('js/SmoothScroll.js')}}"></script>
+   <script src="{{asset('js/owl.carousel.js')}}"></script>
+
+</div>
+  </body>
+</html>
